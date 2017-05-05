8c2052ad3eaa7f799f5abfa468ad5c402f85c5d3
kdemirov <kazim.demirov@gmail.com>
2017-05-05 17:45:19 +0200

    Added Shape 2 commit

---
+++ SpeedBall/SpeedBall/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SpeedBall
+{
+    class Ball
+    {
+        public float Radius { get; set; }
+        public Ball()
+        {
+            Radius = 20;
+        }
+        public void Draw(Graphics g,float X,float Y)
+        {
+     
+            Brush b = new SolidBrush(Color.Blue);
+            g.FillEllipse(b, X, Y, Radius * 2, Radius * 2);
+        }
+       
+    }
+}
---
+++ SpeedBall/SpeedBall/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SpeedBall
+{
+     abstract class Shape
+    {
+       public Point A { get; set; }
+        public Point B { get; set; }
+        public Point C { get; set; }
+        public Point D { get; set; }
+
+
+        abstract public void Draw(Graphics g);
+        abstract public void Move();
+    }
+}
