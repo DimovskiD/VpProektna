da630c7c2838728c26ea8dfde8de9559693c32e9
kdemirov <kazim.demirov@gmail.com>
2017-05-05 17:42:13 +0200

    Add Ball and Shape

--- SpeedBall/SpeedBall/Form1.Designer.cs
+++ SpeedBall/SpeedBall/Form1.Designer.cs
@@ -28,12 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pbGameEngine = new System.Windows.Forms.PictureBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGameEngine)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbGameEngine
+            // 
+            this.pbGameEngine.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbGameEngine.Location = new System.Drawing.Point(12, 12);
+            this.pbGameEngine.Name = "pbGameEngine";
+            this.pbGameEngine.Size = new System.Drawing.Size(415, 711);
+            this.pbGameEngine.TabIndex = 0;
+            this.pbGameEngine.TabStop = false;
+            this.pbGameEngine.Paint += new System.Windows.Forms.PaintEventHandler(this.pbGameEngine_Paint);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(445, 689);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(139, 34);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "New Game";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 736);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.pbGameEngine);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbGameEngine)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pbGameEngine;
+        private System.Windows.Forms.Button btnReset;
     }
 }
 
--- SpeedBall/SpeedBall/Form1.cs
+++ SpeedBall/SpeedBall/Form1.cs
@@ -12,9 +12,17 @@ namespace SpeedBall
 {
     public partial class Form1 : Form
     {
+     private Ball topce;
         public Form1()
         {
             InitializeComponent();
+            topce = new SpeedBall.Ball();
+        }
+
+        private void pbGameEngine_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            topce.Draw(g, pbGameEngine.Width / 2 - topce.Radius, pbGameEngine.Height -2* topce.Radius-40);
         }
     }
 }
