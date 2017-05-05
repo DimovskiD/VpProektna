1783eb315b9e712e7c07cfcc07adc633976b94df
Daniel Dimovski <Daniel Dimovski>
2017-05-05 18:18:37 +0200

    Move rectangle shapes

--- SpeedBall/SpeedBall/Form1.cs
+++ SpeedBall/SpeedBall/Form1.cs
@@ -12,17 +12,50 @@ namespace SpeedBall
 {
     public partial class Form1 : Form
     {
-     private Ball topce;
+         private Ball topce;
+         Timer timer;
+         Timer timerMove;
+         Forms forms = new Forms();
+
+
         public Form1()
         {
+
             InitializeComponent();
             topce = new SpeedBall.Ball();
+
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += timer_Tick;
+            timerMove = new Timer();
+            timerMove.Interval = 100;
+            timerMove.Tick += timerMove_Tick;
+            timer.Start();
+            timerMove.Start();
         }
+
+
+        void timerMove_Tick(object sender, EventArgs e)
+        {
+            forms.move();
+            Invalidate(true);
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            forms.addToForms(new Rectangle(new Point(0,0),30,50));
+            Invalidate();
+        }
+
 
         private void pbGameEngine_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
             topce.Draw(g, pbGameEngine.Width / 2 - topce.Radius, pbGameEngine.Height -2* topce.Radius-40);
+            forms.Draw(e.Graphics);
         }
+
+    
     }
 }
---
+++ SpeedBall/SpeedBall/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedBall
+{
+    class Rectangle : Shape
+    {
+        public int w { get; set; }
+        public int h { get; set; }
+        public Rectangle(Point a,int width, int height)
+        {
+            this.A = a;
+            w = width;
+            h = height;
+            
+        }
+
+
+        public override void Draw(Graphics g)
+        {
+            Brush b = new SolidBrush(Color.Green);
+            g.FillRectangle(b, A.X, A.Y, w, h);
+            
+
+        }
+
+        public override void Move()
+        {
+            A = new Point(A.X, A.Y + 10);
+        }
+    }
+}
